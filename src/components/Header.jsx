441a9c2878d38451c50248857f9c068e9e18fd3a
Devshara <developer.ishara@gmail.com>
2023-04-21 03:05:01 +0100

    add netlify identity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { login, logout } from "../store/actions";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import netlifyIdentity from 'netlify-identity-widget';
+
 
 const Header = () => {
     const user = useSelector(state => state.user);
@@ -29,6 +31,12 @@ const Header = () => {
     }  
     }
 
+    
+netlifyIdentity.init({
+    container: '#netlify-modal', // defaults to document.body
+    locale: 'en' // defaults to 'en'
+  });
+
     return(
         <div className=" m-6">
             <div className=" mx-auto rounded-lg container px-8 py-6 bg-gray-700 text-white flex justify-between items-center">
@@ -36,7 +44,10 @@ const Header = () => {
             <Link to="vote" className=" bg-gray-50 text-gray-900 p-2">Vote an employee</Link>
             <div className="flex items-center gap-6">
                 <h1>{user}</h1>
-                <button onClick={logUser} className=" border p-2">{isLogin ? "Logout" : "Login"}</button>
+                <button onClick={() => {
+                    netlifyIdentity.open(); // open the modal
+
+                }} className=" border p-2">{isLogin ? "Logout" : "Login"}</button>
             </div>
             </div>
         </div>
