b580f34b38abdfb5c41a78f1dd0aed0ebe4d8ad2
Devshara <developer.ishara@gmail.com>
2023-04-21 02:46:12 +0100

    add data to LS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,41 @@
 import { useSelector, useDispatch } from "react-redux";
 import { login, logout } from "../store/actions";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
-    const userName = useSelector(state => state.userName);
+    const user = useSelector(state => state.user);
     const isLogin = useSelector(state => state.isLogin);
+    const employees = useSelector(state => state.employees);
+
+
+    const userId = 102
+
     const dispatch = useDispatch();
 
     function logUser(e){
-        e.preventDefault();
-        
-        if(!isLogin){
-            dispatch(login(102));
-        }else{
-            dispatch(logout())            
-        }  
-    }
+        e.preventDefault(); 
 
-   
+    if(!isLogin){
+        employees.forEach(employee => {
+            console.log(employee)
+            if(employee.empId == userId){                    
+                dispatch(login(employee.empId));
+                
+            }
+        })
+    }else{
+        dispatch(logout())            
+    }  
+    }
 
     return(
         <div className=" m-6">
             <div className=" mx-auto rounded-lg container px-8 py-6 bg-gray-700 text-white flex justify-between items-center">
             <Link to="/"  className=" font-bold text-xl">Employee Rewarder</Link>
-            <Link to="vote" className=" bg-gray-50 text-gray-900 p-2">Rate an employee</Link>
+            <Link to="vote" className=" bg-gray-50 text-gray-900 p-2">Vote an employee</Link>
             <div className="flex items-center gap-6">
-                <h1>{userName}</h1>
+                <h1>{user}</h1>
                 <button onClick={logUser} className=" border p-2">{isLogin ? "Logout" : "Login"}</button>
             </div>
             </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,33 +1,33 @@
 import {LOGIN, LOGOUT, EMPLOYEE_RATED, VOTE_OVER } from "./actions"
 
-export const initialState = JSON.parse(localStorage.getItem('store'));
+export const initialState = 
 
 
-// {userName: '', isLogin:false, employees: [
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 100, vote: 25, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 101, vote: 26, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 102, vote: 50, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 103, vote: 35, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 104, vote: 85, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 105, vote: 95, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
-//     {name: "First Name Last Name ", designation: "Graphic Designer", empId: 106, vote: 15, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+{user: '',  isLogin:false, employees: [
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 100, vote: 25, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 101, vote: 26, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 102, vote: 50, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 103, vote: 35, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 104, vote: 85, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 105, vote: 95, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
+    {name: "First Name Last Name ", designation: "Graphic Designer", empId: 106, vote: 15, voteAvailable:true, suggestions: [], attendence: 56, "jobRes": 7, empVotes:0},
 
-// ]}
+]}
 
 // JSON.parse(localStorage.getItem('store'));
 
 
 export const reducer = (state = initialState, action) => {
     if(action.type == LOGIN){
-        return {...state, isLogin:true, userName: action.payload};
+        return {...state, isLogin:true, user: action.payload};
     }
  
     if(action.type == LOGOUT){
-        return {...state, isLogin:false, userName:null };
+        return {...state, isLogin:false, user:null };
     }
 
     if(action.type == EMPLOYEE_RATED){
-        return {userName:state.userName, isLogin:state.isLogin,  employees: state.employees.map(item => {
+        return {user:state.user, isLogin:state.isLogin,  employees: state.employees.map(item => {
             if(item.empId === action.payload.empId){
                 return {...item, empId:action.payload.empId, vote:item.vote + action.payload.vote}
             }
@@ -37,8 +37,8 @@ export const reducer = (state = initialState, action) => {
     }
 
     if(action.type == VOTE_OVER){
-        return {userName:state.userName, isLogin:state.isLogin,  employees: state.employees.map(item => {
-            if(item.empId === action.payload.empId){
+        return {user:state.user, isLogin:state.isLogin,  employees: state.employees.map(item => {
+            if(item.empId === action.payload){
                 return {...item, voteAvailable:false}
             }
             return item
