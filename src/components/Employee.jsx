362756641436bfe7fd003495b26891480e3a90a0
Devshara <developer.ishara@gmail.com>
2023-05-12 03:00:53 +0100

    fix votesFromEmployees not updating issue

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -5,7 +5,7 @@ const Employee = ({employee}) => {
             <div className="flex gap-6 justify-center border-b-2 p-2" >
                 <h2 >{employee.name}</h2>
                 <h2 >{employee.designation}</h2>
-                <h2 >{employee.totalScore}</h2>
+                <h2 >{employee.votesFromEmployees}</h2>
             </div>
         </li>
     )
